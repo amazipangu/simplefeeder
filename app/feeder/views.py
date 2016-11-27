55c78ac4e80dc49f8ff0f36a1ce215155470e240
Shinya <s.pulscord@gmail.com>
2016-11-27 18:30:00 +0900

    parsing a feed from a remote URL * a remote URL is hard coding

--- app/feeder/views.py
+++ app/feeder/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+import feedparser
+from pprint import pprint
 
-# Create your views here.
+def index(request):
+    url = 'http://rss.rssad.jp/rss/itm/1.0/netlab.xml'
+    feeder = feedparser.parse(url)
+
+    for entry in feeder[entries]:
+        title = entry['title']
+        summary = entry['summary']
+        link = entry['link']
+
+        context = {
+            'title':title,
+            'summary': summary,
+            'link': link,
+        }
+
+    return render(request, 'index.html', context)
