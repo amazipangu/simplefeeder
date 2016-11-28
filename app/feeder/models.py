bb390509c5627cc34c2bba85c884d54d712148f9
Shinya <s.pulscord@gmail.com>
2016-11-28 12:47:21 +0900

    Add Service and Entry model * migrate Service and Entry

--- app/feeder/models.py
+++ app/feeder/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+    created_at = models.DateTimeField('created at')
+
+class Entry(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.CharField(max_length=500)
+    link = models.URLField(max_length=200)
+    created_at = models.DateTimeField('created_at')
